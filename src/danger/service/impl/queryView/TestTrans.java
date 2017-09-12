51bcc124537cdee5b9582a1709dc41a2a3b68c82
qiaozhi <qiao_liqiang@163.com>
2017-09-12 10:29:29 +0800

    测试事务

--- src/danger/service/impl/queryView/TestTrans.java
+++ src/danger/service/impl/queryView/TestTrans.java
@@ -5,7 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+/**
+ * 测试事务
+ * @author QizoLiQiang
+ * @time 2017年9月12日上午10:29:08
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring/applicationContext-*.xml")
 public class TestTrans {
